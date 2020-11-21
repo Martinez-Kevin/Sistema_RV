5cca17f19d391258504e0d69b521efb65902d47b
Martinez-Kevin <70768699+Martinez-Kevin@users.noreply.github.com>
2020-11-21 05:55:29 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sistema_RV.Controllers
+{
+    [Authorize (Roles = "Administrador")]
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        
+        public ActionResult Admin()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sistema_RV.Controllers
+{
+    [Authorize(Roles = "Usuario")]
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        
+        public ActionResult Usuario()
+        {
+            return View();
+        }
+    }
+}
